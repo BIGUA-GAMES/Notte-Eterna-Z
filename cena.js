e99e6f7a6de822cfc02d28d069b54769678adab9
Cassiano Soares <cassiano.ss@aluno.ifsc.edu.br>
2023-03-27 12:00:26 +0000

    Programar as telas de abertura e encerramento #18

---
+++ cena.js
@@ -0,0 +1,11 @@
+export default class CenaDeAbertura extends Phaser.Scene {
+  constructor() {
+    super("abertura");
+  }
+
+  preload() {}
+
+  create() {}
+
+  update() {}
+}
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import config from "./config.js";
+import CenaDeAbertura from "./cena.js";
+import principal from "./principal.js"
+import encerramento from "./encerramento.js"
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    this.scene.add("abertura", CenaDeAbertura);
+    this.scene.add("principal", principal);
+    this.scene.add("encerramento", encerramento);
+    this.scene.start("abertura");
+  
   }
 }
 
